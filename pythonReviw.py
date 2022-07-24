d7a3a5d5565ffb53ca2a1d262109f5d41a49d16b
Layanza14-meet <109899454+Layanza14-meet@users.noreply.github.com>
2022-07-24 23:31:06 +0300

    Create pythonReviw.py

---
+++ pythonReviw.py
@@ -0,0 +1,69 @@
+def create_youtube_video (title,description):
+
+	title=input("the title is:")
+	description=input("the description is:")
+	
+
+
+
+	 video={"title":title,"description":description,"likes":0, "dislikes":0,"comments":{}}
+
+	 return video
+
+
+def like (video):
+	if "likes" in video:
+		video["likes"]+=1
+		return video
+
+def like (video):
+	if "dislikes" in video:
+		video["dislikes"]+=1
+		return video
+
+
+def add_comment(youtubevideo,username,comment_text)
+
+     youtubevideo["comments"][username]=comment_text
+
+
+     return comments
+
+
+
+newvid=create_youtube_video("loveme","a song offerd to the fans ")
+for i in range(496):
+	like(newvid)
+print(newvid)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
+
+
+	
